4fb2fb9121dc99312c019e8903bee8c9afe0f8ec
luiz_gustavo234 <gustavogritte@hotmail.com>
2020-03-26 17:26:26 -0300

    Front-End Application

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,7 +10,7 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ong', OngController.create);
+routes.post('/ongs', OngController.create);
 
 routes.get('/profiles', ProfileController.index);
 
